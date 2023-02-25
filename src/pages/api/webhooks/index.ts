3f053716838425c7689c63b2dadf3d2091da5e60
Ashish Suman <ashish.suman@3pilarglobal.com>
2023-02-25 19:49:20 +0530

    Added payment method

--- src/pages/api/webhooks/index.ts
+++ src/pages/api/webhooks/index.ts
@@ -144,7 +144,7 @@ export class WebhooksController extends BaseBigCommerceController {
     const subscription = await this.stripeService.createSubscription({
       customer: stripe_customer_id,
       items: subscription_items,
-      // default_payment_method: customer_payment_id,
+      default_payment_method: customer_payment_id,
       metadata: {
         bc_order_id: order_id
       }
