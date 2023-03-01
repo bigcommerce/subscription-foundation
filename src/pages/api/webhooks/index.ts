a56a4747fa77eee1c8afeb67bcc2091b6a844808
sakshi-babbar <sakshi.babbar@3pillarglobal.com>
2023-03-01 17:58:46 +0530

    code refactor

--- src/pages/api/webhooks/index.ts
+++ src/pages/api/webhooks/index.ts
@@ -118,7 +118,7 @@ export class WebhooksController extends BaseBigCommerceController {
     //   }
     // }
 
-    subscription_items.push({ price: "price_1MedXOEr4p5zg8WUq6gPbY3f" });
+    subscription_items.push({ price: process.env.STRIPE_SUBSCRIPTION_PRICE });
 
     console.log("subscription_items ---------->", subscription_items);
 
